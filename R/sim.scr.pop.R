250e9f701849b532ca78761d17094f00aa544318
m-murchie <mpmm@st-andrews.ac.uk>
2017-02-16 17:54:16 +0000

    changed phi to 1-phi in rgeom function

--- R/sim.scr.pop.R
+++ R/sim.scr.pop.R
@@ -3,7 +3,8 @@
 library(secr)
 library(fields)
 
-sim.scr.pop <- function(lambda0, sigma, occasions, seed=123) {
+sim.open.scr.pop <- function(lambda0, sigma, occasions, phi, D = 0.1, 
+                             seed=12345) {
   
   # create the detectors
   detype <- "count"
@@ -12,11 +13,6 @@ sim.scr.pop <- function(lambda0, sigma, occasions, seed=123) {
   
   # create the mesh
   mesh <- make.mask(dets, buffer=4*sigma, nx=64, ny=64, type="trapbuffer")
-  
-  # set density via abundance on mesh
-  Nmesh <- 100
-  M <- dim(mesh)[1]
-  D <- Nmesh / (M * attributes(mesh)$area)
   
   # simulate a population from this intensity surface
   pop <- sim.popn(D=D, core=mesh, buffer=0, seed=seed)
@@ -30,28 +26,25 @@ sim.scr.pop <- function(lambda0, sigma, occasions, seed=123) {
   plot(mesh, border=5,dots=FALSE, col="white", meshcol="gray")
   points(pop$x,pop$y,pch=19,cex=0.25)
   plot(dets,add=TRUE)
-  plot(capthist, border=sigma, tracks=TRUE, gridlines=FALSE,rad=3,add=TRUE)
+
+  # randomly generate survival times with known survival probability
+  n <- dim(capthist)[1]
+  set.seed(seed)
+  survival.occ <- rgeom(n, 1-phi)
   
-  return(list(mesh=mesh, capthist=capthist))
-  
+  # thin capture history 
+  for (i in 1:n) {
+    if (survival.occ[i] < occasions) {
+      capthist[i, (survival.occ[i]+1):occasions, ] <- 0
+    }
+  }
+  return(list("capthist"=capthist, "mesh"=mesh))
 }
 
 # simulate an example population
-lambda0 <- 2 ; sigma <- 20 ; occasions <- 5
-scr.pop <- sim.scr.pop(lambda0=lambda0, sigma=sigma, occasions=occasions)
-pop.capthist <- scr.pop$capthist
-mesh <- scr.pop$mesh
+lambda0 <- 0.1 ; sigma <- 800 ; occasions <- 20 ; phi <- 0.9 ; D <- 1
+sim <- sim.open.scr.pop(lambda0, sigma, occasions, phi)
+capthist.sim <- sim$capthist
+mesh <- sim$mesh
 
-# randomly generate survival times with known survival probability
-n <- dim(pop.capthist)[1]
-phi <- 0.5
-set.seed(123)
-survival.occ <- rgeom(n, phi)
-
-# thin capture history 
-for (i in 1:n) {
-  if (survival.occ[i] < occasions) {
-    pop.capthist[i, (survival.occ[i]+1):occasions, ] <- 0
-  }
-}
-
+plot(capthist.sim, border=sigma, tracks=TRUE, gridlines=FALSE, rad=3, add=TRUE)
